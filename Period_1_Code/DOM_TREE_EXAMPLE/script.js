78c82224d00bc06966ead16a299f1ca16bff1324
trudwick <trudwick@gmail.com>
2017-02-28 14:09:59 +0000

    Added Event code for DOM

---
+++ Period_1_Code/DOM_TREE_EXAMPLE/script.js
@@ -0,0 +1,7 @@
+function wasClicked(evt){
+    // console.log("Body was clicked");
+    // console.log(evt)
+    var tgt = evt.target;
+    console.log(tgt)
+    console.log(tgt.children)
+}
---
+++ Period_7_Code/DOMTreeExample/script.js
@@ -0,0 +1,7 @@
+function clickFunction(event){
+    console.log(event);
+    var tgt = event.target;
+    var children = tgt.children
+    console.log(children)
+    // console.log(tgt)
+}
